5cf661f1bdfd78d06ff09a631e552cd7c7ced940
Rohan Parkes <rparkes@email.com>
2026-01-20 14:03:17 +1100

    Fixed failure to hash issue.

--- models/User.js
+++ models/User.js
@@ -1,8 +1,19 @@
-import { Schema, mongoose } from 'mongoose';
+import bcrypt from 'bcrypt';
+import { mongoose } from 'mongoose';
+
+const Schema = mongoose.Schema;
 
 const UserSchema = new Schema({
   username: String,
   password: String,
+});
+
+UserSchema.pre('save', async function () {
+  // let user = this;
+  console.log(this.password);
+  this.password = await (async () => {
+    return bcrypt.hash(this.password, 10);
+  })();
 });
 
 const User = mongoose.model('User', UserSchema);
